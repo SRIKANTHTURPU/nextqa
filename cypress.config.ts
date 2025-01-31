8a496b50277a8750d508505498f1e540204eb6be
Srikanth Turpu <srikanth.turpu@nomihealth.com>
2025-01-31 09:11:03 +0530

    Added Report Generation Functionality

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,16 +2,37 @@ import { defineConfig } from "cypress";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
 import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
 import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
-require('dotenv').config();
+require("dotenv").config();
 
 export default defineConfig({
   e2e: {
     specPattern: "cypress/e2e/features/**/*.feature", // Path to feature files
     supportFile: "cypress/support/e2e.ts",
+    env: {
+      ...process.env,
+      stepDefinitions: "cypress/e2e/step-definitions/**/*.cy.{js,ts}",
+    },
+    // 👇 Define where Cypress should look for step definitions
+    experimentalRunAllSpecs: true,
+    // ✅ Override existing reports instead of generating new ones
+    reporter: "mocha-multi-reporters",
+    reporterOptions: {
+      reporterEnabled: "mochawesome, mocha-junit-reporter",
+      mochawesomeReporterOptions: {
+        reportDir: "cypress/reports/mochawesome",
+        overwrite: true, // 👈 Ensure reports are overridden
+        html: true,
+        json: true,
+      },
+      mochaJunitReporterReporterOptions: {
+        mochaFile: "cypress/reports/junit/results.xml", // 👈 Fixed to override instead of creating new files
+        toConsole: true,
+      },
+    },
+
     setupNodeEvents: async (on, config) => {
       // Add Cucumber Preprocessor Plugin
       await addCucumberPreprocessorPlugin(on, config);
-
       // Set up Esbuild for preprocessing
       on(
         "file:preprocessor",
@@ -20,13 +41,8 @@ export default defineConfig({
         })
       );
 
+      
       return config; // Return updated config
-    },
-    // 👇 Define where Cypress should look for step definitions
-    experimentalRunAllSpecs: true,
-    env: {
-      ...process.env,
-      stepDefinitions: "cypress/e2e/step-definitions/**/*.cy.{js,ts}",
     },
   },
 });
